9365ee28adb7544a6fc886caffe1341e8c820c8f
Kesley DEV <kesleygti@gmail.com>
2022-02-21 19:16:48 -0300

    feat: font roboto

--- App.tsx
+++ App.tsx
@@ -1,6 +1,23 @@
 import { Home } from './src/screens/Home';
 
+import { 
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+
+import AppLoading from 'expo-app-loading';
+
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <Home />
   );
